f71523b0f9f2178edf13523d5a4f27142a0c7563
Ivan <ivan.turutin@gmail.com>
2022-07-23 18:22:31 +0300

    7. Получение Java объектов из БД

--- src/main/java/com/springcourse/hibernatetest/Test2.java
+++ src/main/java/com/springcourse/hibernatetest/Test2.java
@@ -5,23 +5,29 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
-
         try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory()) {
             Session session = factory.getCurrentSession();
 
-            Employee employee = new Employee("Ivan", "Turutin",
-                    "DPHC", 160);
-            Employee employee1 = new Employee("Man", "Human",
-                    "XYZ", 110);
+            Employee employee = new Employee("Oleg", "Trugin",
+                    "IT", 250);
             session.beginTransaction();
-            session.save(employee1);
+            session.save(employee);
+//            session.getTransaction().commit();
+
+            int myId = employee.getId();
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+            Employee employee1 = session.get(Employee.class, myId);
             session.getTransaction().commit();
+
             System.out.println(employee1);
+
+
         }
 
     }
--- src/main/java/com/springcourse/hibernatetest/Test3.java
+++ src/main/java/com/springcourse/hibernatetest/Test3.java
@@ -5,23 +5,29 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class Test3 {
+    public static void main(String[] args) {
         try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory()) {
             Session session = factory.getCurrentSession();
 
-            Employee employee = new Employee("Ivan", "Turutin",
-                    "DPHC", 160);
-            Employee employee1 = new Employee("Man", "Human",
-                    "XYZ", 110);
             session.beginTransaction();
-            session.save(employee1);
+
+/*
+            List<Employee> emps = session.createQuery("from Employee")
+                    .getResultList();
+*/
+            List<Employee> emps = session.createQuery("from Employee "
+                    + "where name = 'Ivan1' and salary > 160").getResultList();
+            for (Employee employee : emps) {
+                System.out.println(employee);
+            }
             session.getTransaction().commit();
-            System.out.println(employee1);
+
         }
 
     }
