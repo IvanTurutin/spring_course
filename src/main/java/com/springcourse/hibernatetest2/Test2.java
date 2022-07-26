06fcbf827a56da3f6452c0de92ed7b70a338ccfb
Ivan <ivan.turutin@gmail.com>
2022-07-26 20:11:44 +0300

    13. Отношение Один-к-Одному (Bi-directional)

---
+++ src/main/java/com/springcourse/hibernatetest2/Test2.java
@@ -0,0 +1,58 @@
+package com.springcourse.hibernatetest2;
+
+import com.springcourse.hibernatetest2.entity.Detail;
+import com.springcourse.hibernatetest2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+             Session session = factory.getCurrentSession()) {
+
+/*
+            Employee employee = new Employee("Misha", "Korovin",
+                    "123", 200);
+
+            Detail detail = new Detail("SPb", "753456",
+                    "mih@gmail.com");
+
+            employee.setEmpDetail(detail);
+            detail.setEmployee(employee);
+
+            session.beginTransaction();
+
+            session.save(detail);
+
+            session.getTransaction().commit();
+*/
+
+
+/*
+            session.beginTransaction();
+
+            Detail detail = session.get(Detail.class, 11);
+            Employee emp = detail.getEmployee();
+            System.out.println(detail);
+            System.out.println(emp);
+
+            session.getTransaction().commit();
+*/
+
+
+            session.beginTransaction();
+
+            session.delete(session.get(Detail.class, 10));
+
+            session.getTransaction().commit();
+
+        }
+
+    }
+}
--- src/main/java/com/springcourse/hibernatetest2/entity/Detail.java
+++ src/main/java/com/springcourse/hibernatetest2/entity/Detail.java
@@ -17,6 +17,18 @@ public class Detail {
     @Column(name = "email")
     private String email;
 
+    @OneToOne(mappedBy = "empDetail",
+    cascade = CascadeType.ALL)
+    private Employee employee;
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
     public Detail(String city, String phoneNumber, String email) {
         this.city = city;
         this.phoneNumber = phoneNumber;
